d49c5795ffd2bf67ff76838915810256ab3d1505
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-06-06 23:24:47 +0100

    credits

--- script/model/player.js
+++ script/model/player.js
@@ -1,6 +1,10 @@
 define(function () {
     var Player = function () {
-        
+        this.credits = 0;
+    };
+
+    Player.prototype.addCredits = function (credits) {
+        this.credits += credits;
     };
 
     return Player;
--- test/model/player.js
+++ test/model/player.js
@@ -1,6 +1,12 @@
 require(['model/player'], function (Player){
 
-    module('model/player');
+    var player;
+
+    module('model/player', {
+        setup: function () {
+            player = new Player();
+        }
+    });
 
     test("should exist", function () {
         ok(Player);
@@ -8,6 +14,15 @@ require(['model/player'], function (Player){
 
     test("should be instantiatable", function () {
         ok(new Player());
+    });
+
+    test("should start with 0 credits", function () {
+        equal(player.credits, 0);
+    });
+
+    test("should add credits", function () {
+        player.addCredits(1);
+        equal(player.credits, 1);
     });
     
 });
